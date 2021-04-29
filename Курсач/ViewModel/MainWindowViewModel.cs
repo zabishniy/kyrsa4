15370ea2c08230aadd7fb2e684ff1ba3360ae4da
zabishniy <kzabishniy@gmail.com>
2021-04-29 14:07:39 +0300

    kyrsa4

---
+++ Курсач/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Курсач.Model;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Курсач.ViewModel
+{
+    class MainWindowViewModel :INotifyPropertyChanged
+    {
+        public Champ[] Champs { get; private set; }
+        Champ selectedChamp;
+        public Champ SelectedChamp 
+        {
+            get { return selectedChamp; }
+            set
+            {
+                selectedChamp=value;
+                OnPropertyChanged("SelectedChamp"); 
+            }
+        }
+        public MainWindowViewModel()
+        {
+            Champs = Champ.GetChamps();
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged([CallerMemberName] string propertyName ="")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
